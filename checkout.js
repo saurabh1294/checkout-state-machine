a493360e310e9f374adf97f07ef212c66826c9df
Saurabh Gattani <saurabh.gattani@gmail.com>
2019-05-31 23:40:58 +1000

    code formatting

--- checkout.js
+++ checkout.js
@@ -1,12 +1,11 @@
 var readline = require('readline');
-	
-const productMap = {
-	'ipd' : '$549.99',
-	'mbp' : '$1399.99',
-	'atv' : '$109.5',
-	'vga' : '$30'
-}
 
+const productMap = {
+	'ipd': '$549.99',
+	'mbp': '$1399.99',
+	'atv': '$109.5',
+	'vga': '$30'
+}
 
 const outputMap = {};
 let total = 0;
@@ -14,11 +13,9 @@ let stdin = process.openStdin();
 let totalMap = {};
 
 
-
 // Prompt user to input data in console.
 console.log("Please input product code out of atv, ipd, mbp, vga");
 
-	
 stdin.addListener("data", function(input) {
 	let key = input.toString().replace(/\r?\n/, '');
 
@@ -30,64 +27,86 @@ stdin.addListener("data", function(input) {
 	const atvThreshold = 3;
 	const ipdThreshold = 4;
 	let cost = 0;
-	
-	
-	switch(key) {
+
+	switch (key) {
 		case 'atv':
 			if (typeof(totalMap[key]) === 'undefined') {
-				totalMap[key] = {cost : 0, qty : 0};
+				totalMap[key] = {
+					cost: 0,
+					qty: 0
+				};
 			}
 			if (outputMap[key].length && outputMap[key].length % atvThreshold === 0) {
 				cost = (outputMap[key].length - Math.round(outputMap[key].length / atvThreshold)) * price +
-								(outputMap[key].length - atvThreshold * Math.round(outputMap[key].length / atvThreshold)) * price;
+					(outputMap[key].length - atvThreshold * Math.round(outputMap[key].length / atvThreshold)) * price;
 			} else {
 				cost = outputMap[key].length * price;
 			}
-			totalMap[key] = {cost : cost, qty : outputMap[key].length};
+			totalMap[key] = {
+				cost: cost,
+				qty: outputMap[key].length
+			};
 			break;
-			
+
 		case 'mbp':
 			if (typeof(totalMap[key]) === 'undefined') {
-				totalMap[key] = {cost : 0, qty : 0};
+				totalMap[key] = {
+					cost: 0,
+					qty: 0
+				};
 			}
 			cost = outputMap[key].length * price;
-			totalMap[key] = {cost : cost, qty : outputMap[key].length};
+			totalMap[key] = {
+				cost: cost,
+				qty: outputMap[key].length
+			};
 			// check for VGA here
 			if (totalMap['vga'] && outputMap[key].length >= totalMap['vga']['qty']) totalMap['vga']['cost'] = 0;
 			break;
-			
+
 		case 'ipd':
 			if (typeof(totalMap[key]) === 'undefined') {
-				totalMap[key] = {cost : 0, qty : 0};
+				totalMap[key] = {
+					cost: 0,
+					qty: 0
+				};
 			}
 			if (outputMap[key].length >= ipdThreshold) {
 				cost = outputMap[key].length * (price - 50);
 			} else {
 				cost = outputMap[key].length * price;
 			}
-			totalMap[key] = {cost : cost, qty : outputMap[key].length};
+			totalMap[key] = {
+				cost: cost,
+				qty: outputMap[key].length
+			};
 			break;
-			
+
 		case 'vga':
 			if (typeof(totalMap[key]) === 'undefined') {
-				totalMap[key] = {cost : 0, qty : 0};
+				totalMap[key] = {
+					cost: 0,
+					qty: 0
+				};
 			}
 			var mbpCount = outputMap['mbp'] && outputMap['mbp'].length || 0;
 			var vgaCount = outputMap[key].length;
 			var diff = (vgaCount > mbpCount) ? vgaCount - mbpCount : 0;
 			cost = diff * price;
-			totalMap[key] = {cost : cost, qty : outputMap[key].length};
+			totalMap[key] = {
+				cost: cost,
+				qty: outputMap[key].length
+			};
 			break;
 	}
-	
+
 	console.log('Total = ', totalMap);
 	total = 0;
 	for (key in totalMap) {
 		console.log(key, totalMap[key]['cost']);
 		total += totalMap[key]['cost'];
 	}
-	
-	
+
 	console.log('Product final total = ', total);
 	// Prompt user to input data in console.
 	console.log("\nPlease input product code out of atv, ipd, mbp, vga !! Ctrl+ C to exit");
