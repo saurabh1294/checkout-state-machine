994c0e7fc07c9efa07e3704557d90d9711ca96e7
Saurabh Gattani <saurabh.gattani@gmail.com>
2019-06-01 16:13:40 +1000

    code refactoring

--- checkout.js
+++ checkout.js
@@ -7,11 +7,16 @@ const productMap = {
 	'vga': '$30'
 }
 
+const complementaryProductMap = {
+	0 : 'mbp,vga'
+	// Add more here if required
+};
+
 const outputMap = {};
 let total = 0;
 let stdin = process.openStdin();
 let totalMap = {};
-
+const productCodes = ['atv', 'mbp', 'vga', 'ipd'];
 
 // Prompt user to input data in console.
 console.log("Please input product code out of atv, ipd, mbp, vga");
@@ -22,10 +27,17 @@ stdin.addListener("data", function(input) {
 	if (typeof(outputMap[key]) === 'undefined') {
 		outputMap[key] = [];
 	}
+
+	if (!productCodes.includes(key)) {
+		console.log('Please enter a valid product code out of atv,ipd,mbp,vga');
+		return;
+	}
+
 	outputMap[key].push(key);
 	const price = parseFloat(productMap[key].replace('$', ''));
 	const atvThreshold = 3;
 	const ipdThreshold = 4;
+	const complementaryProducts = complementaryProductMap[0].split(',');
 	let cost = 0;
 
 	switch (key) {
@@ -61,7 +73,9 @@ stdin.addListener("data", function(input) {
 				qty: outputMap[key].length
 			};
 			// check for VGA here
-			if (totalMap['vga'] && outputMap[key].length >= totalMap['vga']['qty']) totalMap['vga']['cost'] = 0;
+			if (totalMap[complementaryProducts[1]] && outputMap[key].length >= totalMap[complementaryProducts[1]]['qty']) {
+				totalMap[complementaryProducts[1]]['cost'] = 0;
+			}
 			break;
 
 		case 'ipd':
@@ -89,7 +103,7 @@ stdin.addListener("data", function(input) {
 					qty: 0
 				};
 			}
-			var mbpCount = outputMap['mbp'] && outputMap['mbp'].length || 0;
+			var mbpCount = outputMap[complementaryProducts[0]] && outputMap[complementaryProducts[0]].length || 0;
 			var vgaCount = outputMap[key].length;
 			var diff = (vgaCount > mbpCount) ? vgaCount - mbpCount : 0;
 			cost = diff * price;
@@ -97,6 +111,9 @@ stdin.addListener("data", function(input) {
 				cost: cost,
 				qty: outputMap[key].length
 			};
+			break;
+
+		default:
 			break;
 	}
 
@@ -107,7 +124,8 @@ stdin.addListener("data", function(input) {
 		total += totalMap[key]['cost'];
 	}
 
-	console.log('Product final total = ', total);
+	console.log('Product final total = \x1b[32m \x1b[5m', total);
+
 	// Prompt user to input data in console.
-	console.log("\nPlease input product code out of atv, ipd, mbp, vga !! Ctrl+ C to exit");
+	console.log("\nPlease input product code out of atv, ipd, mbp, vga !! Ctrl+ C to exit\x1b[0m");
 });
